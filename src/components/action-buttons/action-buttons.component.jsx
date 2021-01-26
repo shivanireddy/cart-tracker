350bdda75073ea40725a251092870062c729c7e3
Shivani Reddy <shivanireddy010197@gmail.com>
2021-01-26 02:19:34 -0600

    adding action buttons

---
+++ src/components/action-buttons/action-buttons.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./action-buttons.styles.scss";
+
+export const YesActionButton = ({ actionFunction,callbacks}) => {
+    return (
+        <div className="alert alert-success">
+            <button
+              class="btn btn--action btn--yes"
+              onClick={() => {
+                  actionFunction();
+                  callbacks.forEach((callback) => {
+                      callback();
+                  });
+              }}
+            >
+              Yes
+            </button>
+        </div>
+    );
+};
+
+export const NoActionButton = ({ actionFunction}) => {
+    return (
+        <div className="alert alert--error">
+            <button
+                className="btn btn--action btn--no"
+                onClick={() => actionFunction()}
+            >No</button>
+        </div>
+    );
+};
