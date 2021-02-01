5b6951414862b0768210f0a8b778bd38292851b8
Shivani Reddy <shivanireddy010197@gmail.com>
2021-02-01 00:08:02 -0600

    adding grocery status and item card

---
+++ src/components/item-card/item-card.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import {connect} from react-redux;
+import {addItemToCart} from "../../redux/shopping-cart/shopping-cart.actions";
+
+import plus from "../../asset.plus.svg";
+
+import "./item-card.styles.scss";
+
+const ItemCard = ({ itemDetails, addItemsToCart}) => {
+    const { name } = itemDetails;
+    return (
+        <div className="item__card">
+            <p>{name}</p>
+            <span onClick={() => addItemsToCart(itemDetails)}>
+                <img src={plus} alt="Plus Icon to add items to shopping cart" />
+            </span>
+        </div>
+    );
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addItemsToCart:(item) => dispatch(addItemToCart(item)),
+    };
+};
+
+export default connect(null, mapDispatchToProps)(ItemCard);
