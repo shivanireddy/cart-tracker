fe85b82e9624aa052f5438759a8d09e63d2cbe2c
Shivani Reddy <shivanireddy010197@gmail.com>
2021-02-01 00:36:41 -0600

    Adding item list

---
+++ src/components/item-lists/item-lists.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import ItemCard from "../../components/item-card/item-card.component";
+import "./item-lists.styles.scss";
+
+const ItemLists = ({listDetails}) => {
+    const { categoryName, items} = listDetails;
+    return (
+        <div className="itemlists">
+            <h2 className="itemlists__category">{categoryName}</h2>
+            <div className="itemlists__list">
+                {items.map((item) => {
+                    return <ItemCard key = {item.id} itemDetails={item} />;
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default ItemLists;
